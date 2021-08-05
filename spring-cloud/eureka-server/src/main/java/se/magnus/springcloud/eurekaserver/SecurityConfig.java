c30e5c1cf94b87b4468fc8ef6dad57015526b9f4
Vikas Warkar <vikaswarkar@gmail.com>
2021-08-05 15:38:19 -0500

    Added https suport to gateway. Added basic auth to eureka server. Configured eureka clients to provide username:password to connect to the eureka server.

---
+++ spring-cloud/eureka-server/src/main/java/se/magnus/springcloud/eurekaserver/SecurityConfig.java
@@ -0,0 +1,41 @@
+package se.magnus.springcloud.eurekaserver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final String username;
+    private final String password;
+
+    @Autowired
+    public SecurityConfig(@Value("${app.eureka-username}") String username, @Value("${app.eureka-password}") String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder amb) throws Exception {
+        amb.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser(username).password(password)
+                .authorities("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+}
