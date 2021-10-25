0159f9859f60bef5010dfdd974127a3563c5f763
Vikas Warkar <vikaswarkar@gmail.com>
2021-10-25 09:55:57 -0500

    Gateway added in the mix - working in local

--- spring-cloud/gateway/src/main/java/se/magnus/microservices/springcloud/gateway/GatewaySecurityConfig.java
+++ spring-cloud/gateway/src/main/java/se/magnus/microservices/springcloud/gateway/GatewaySecurityConfig.java
@@ -1,24 +1,24 @@
 package se.magnus.microservices.springcloud.gateway;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-@EnableWebSecurity
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//@EnableWebSecurity
 public class GatewaySecurityConfig {
 
-    @Bean
-    SecurityWebFilterChain gatewaySecurityFilterChain(ServerHttpSecurity httpSecurity){
-        httpSecurity.csrf().disable()
-                .authorizeExchange()
-                .pathMatchers("/actuator/**").permitAll()
-                .pathMatchers("/eureka/*").permitAll()
-                .pathMatchers("/oauth/**").permitAll()
-                .anyExchange().authenticated()
-                .and()
-                .oauth2ResourceServer()
-                .jwt();
-        return httpSecurity.build();
-    }
+//    @Bean
+//    SecurityWebFilterChain gatewaySecurityFilterChain(ServerHttpSecurity httpSecurity){
+//        httpSecurity.csrf().disable()
+//                .authorizeExchange()
+//                .pathMatchers("/actuator/**").permitAll()
+//                .pathMatchers("/eureka/*").permitAll()
+//                .pathMatchers("/oauth/**").permitAll()
+//                .anyExchange().authenticated()
+//                .and()
+//                .oauth2ResourceServer()
+//                .jwt();
+//        return httpSecurity.build();
+//    }
 }
